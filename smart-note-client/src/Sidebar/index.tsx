099310680fb4e1a51a385c7371abc90b14a5278b
ThaiHaiDev <thaihai1004@gmail.com>
2022-07-15 11:29:38 +0700

    HA-9: create interface profile, search in sidebar

--- smart-note-client/src/Sidebar/index.tsx
+++ smart-note-client/src/Sidebar/index.tsx
@@ -1,10 +1,18 @@
 import styles from './Sidebar.module.scss';
 import classNames from 'classnames/bind';
+import Profile from './Profile';
+import Search from './Search';
 
 const cx = classNames.bind(styles);
 
 const SideBar = () => {
-    return <div className={cx('wrapper')}>Sidebar</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <img src="https://raw.githubusercontent.com/ThaiHaiDev/StoreImage/main/logo.png" alt="Avatar" style={{ width: '50px', height: '50px' }}></img>
+            <Profile />
+            <Search />
+        </div>
+    );
 };
 
 export default SideBar;

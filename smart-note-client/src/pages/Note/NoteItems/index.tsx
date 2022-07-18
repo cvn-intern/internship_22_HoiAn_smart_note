5d9c20ce48e1e69fb88fc45a4cae0250f44386a9
Tran Thi Cam Giang <gtran527@gmail.com>
2022-07-18 08:52:39 +0700

    HA-7: code UI create note #2

--- smart-note-client/src/pages/Note/NoteItems/index.tsx
+++ smart-note-client/src/pages/Note/NoteItems/index.tsx
@@ -1,46 +1,47 @@
 import classNames from 'classnames/bind';
-import { FileAddOutlined } from '@ant-design/icons'
-import styles from './NoteItems.module.scss'
+import { FileAddOutlined } from '@ant-design/icons';
+import styles from './NoteItems.module.scss';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 interface notes {
-    notes: any,
-    onAddNote: any,
-    onDeleteNote: any,
-    activeNote: any,
-    setActiveNote: any,
+    notes: any;
+    onAddNote: any;
+    onDeleteNote: any;
+    activeNote: any;
+    setActiveNote: any;
 }
 
-const NoteItems = ({notes, onAddNote, onDeleteNote, activeNote, setActiveNote }: notes) => {
+const NoteItems = ({ notes, onAddNote, onDeleteNote, activeNote, setActiveNote }: notes) => {
+    const sortedNotes: any = notes.sort((a: any, b: any) => {
+        return b.lastModified - a.lastModified;
+    });
 
-    return <div  className={cx('container')}>
-        <header className={cx('header')}>
-            <div className={cx('header-content')}>
-                <h3>Uncategory</h3>
-                <FileAddOutlined className={cx('add-icon')}  onClick={onAddNote}/>
-            </div>
-            <div className={cx('header-totalnote')}>
-                4 notes
-            </div>
-        </header>
-        {notes.map((note : any) => (
-            <div className={cx('note', 'active')} key={note.user_id} onClick={() => setActiveNote(note.user_id)}>
-                <div className={cx('noteTitle')}>
-                    <strong>{note.title}</strong>
-                    <button onClick={() => onDeleteNote(note.user_id)}>Delete</button>
+    return (
+        <div className={cx('container')}>
+            <header className={cx('header')}>
+                <div className={cx('header-content')}>
+                    <h3>Uncategory</h3>
+                    <FileAddOutlined className={cx('add-icon')} onClick={onAddNote} />
                 </div>
-                <p>{note.body && note.body.substr(0, 100) + "..."}</p>
-                <small>
-                    Last Modilfied{""}
-                    {new Date(note.lastModified).toLocaleDateString("en-GB", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                })}
-                </small>
-            </div>
-        ))}
-    </div>;
+                <div className={cx('header-totalnote')}>4 notes</div>
+            </header>
+            {sortedNotes.map((note: any) => (
+                <div
+                    className={cx('note', `${note.id === activeNote && 'active'}`)}
+                    key={note.id}
+                    onClick={() => setActiveNote(note.id)}
+                >
+                    <div className={cx('noteTitle')}>
+                        <strong>{note.title && note.title.substr(0, 10) + '...'}</strong>
+                        <button onClick={() => onDeleteNote(note.id)}>Delete</button>
+                    </div>
+                    <p>{note.body && note.body.substr(0, 20) + '...'}</p>
+                    <small>{new Date(note.lastModified).toLocaleDateString()}</small>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default NoteItems;
--- smart-note-client/src/pages/Note/index.tsx
+++ smart-note-client/src/pages/Note/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import classNames from 'classnames/bind';
 import styles from './Note.module.scss';
 import NoteItems from './NoteItems';
@@ -8,41 +8,55 @@ import NoteDetails from './NoteDetails';
 const cx = classNames.bind(styles);
 
 const Note = () => {
-    const [ notes, setNotes ] = useState(
-        localStorage.notes ? JSON.parse(localStorage.notes) : []
-    )
-    
-    const [activeNote, setActiveNote] = useState(false)
+    const [notes, setNotes] = useState(localStorage.notes ? JSON.parse(localStorage.notes) : []);
+
+    const [activeNote, setActiveNote] = useState(false);
 
     useEffect(() => {
-        localStorage.setItem("notes", JSON.stringify(notes))
-    }, [notes])
+        localStorage.setItem('notes', JSON.stringify(notes));
+    }, [notes]);
 
-    const onAddNote =  () => {
+    const onAddNote = () => {
         const newNote = {
-            user_id: uuidv4(),
-            title: "Untitle note",
-            body: "",
+            id: uuidv4(),
+            title: 'Untitle note',
+            body: '',
             lastModified: Date.now(),
-        }
+        };
 
-        setNotes([newNote, ...notes])
-    }
+        setNotes([newNote, ...notes]);
+    };
 
-    const onDeleteNote = (idToDelete : number) => {
-        setNotes(notes.filter((note : any) => note.user_id !== idToDelete))
-    }
+    const onUpdateNote = (updatedNote: any) => {
+        const updatedNotesArray = notes.map((note: any) => {
+            if (note.id === activeNote) {
+                return updatedNote;
+            }
+
+            return note;
+        });
+
+        setNotes(updatedNotesArray);
+    };
+
+    const onDeleteNote = (idToDelete: number) => {
+        setNotes(notes.filter((note: any) => note.id !== idToDelete));
+    };
+
+    const getActiveNote = () => {
+        return notes.find((note: any) => note.id === activeNote);
+    };
 
     return (
         <div className={cx('wrapper-content')}>
-            <NoteItems 
-                notes={notes} 
-                onAddNote={onAddNote} 
+            <NoteItems
+                notes={notes}
+                onAddNote={onAddNote}
                 onDeleteNote={onDeleteNote}
                 activeNote={activeNote}
                 setActiveNote={setActiveNote}
             />
-            <NoteDetails/>
+            <NoteDetails activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
         </div>
     );
 };

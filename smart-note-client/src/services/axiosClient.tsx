778803a35a38172751f4eee86bbeb84dd9a002ee
ThaiHaiDev <thaihai1004@gmail.com>
2022-07-19 09:05:49 +0700

    HA-12: fix env

--- smart-note-client/src/services/axiosClient.tsx
+++ smart-note-client/src/services/axiosClient.tsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
-// import process from 'process';
+import process from 'process';
 
-// const API_BASE_URL=process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL=process.env.REACT_APP_API_BASE_URL;
 
 const axiosClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
